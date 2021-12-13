48b7da53752f2d6c2fd9bcbeab6ff3a30fbd3329
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-12-13 09:02:33 +0100

    created form template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Message;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -17,7 +20,9 @@ class HomeController extends AbstractController
      */
     public function index(PhotoRepository $photoRepository): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
 
-        return $this->render('home/index.html.twig', ['photos' => $photoRepository->findAll()]);
+        return $this->render('home/index.html.twig', ['photos' => $photoRepository->findAll(), 'form' => $form->createView()]);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addStyleEntry('userPhoto', './assets/styles/user/photo.scss')
+    .addStyleEntry('home','./assets/styles/home/index.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
